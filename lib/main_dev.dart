b2dfbf7c9fccb337f11af6b94b16e23770abaa81
Michel <michelsousa7@gmail.com>
2020-09-05 01:55:12 -0300

    Atuliazando Imports Domanin

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutterExpenses/src/domain/providers/infra/repository/providerRepository.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'app.dart';
+import 'src/presentation/app.dart';
 
 Future<void> main() async {
   await DotEnv().load('.env_dev');
   runApp(App());
-  print('addll');
+  var repo = ProviderRepository().transactionDAO().count();
+  repo.then((value) => print(value));
 }
--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-import 'app.dart';
+import 'src/presentation/app.dart';
 
 Future<void> main() async {
   await DotEnv().load('.env');
